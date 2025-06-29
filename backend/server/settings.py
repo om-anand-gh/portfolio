f91483b6e1323e7625d7216d0d77af575dd09fc4
om-anand-gh <om.anand@dal.ca>
2025-06-29 15:41:49 -0300

    chore(docker): add Dockerfile, Compose, and VSCode setup for containerization

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", default="localhost,127.0.0.1")
+ALLOWED_HOSTS = ALLOWED_HOSTS.split(",")
 
 
 # Application definition
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -83,11 +85,11 @@ WSGI_APPLICATION = "server.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "mssql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
+        "NAME": env("DB_NAME", default="DB_NAME"),
+        "USER": env("DB_USER", default="DB_USER"),
+        "PASSWORD": env("DB_PASSWORD", default="DB_PASSWORD"),
+        "HOST": env("DB_HOST", default="DB_HOST"),
+        "PORT": env("DB_PORT", default="DB_PORT"),
         "OPTIONS": {
             "driver": "ODBC Driver 18 for SQL Server",
         },
@@ -130,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
